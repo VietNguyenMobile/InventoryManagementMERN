779034cd116cf1450169e02765bfb9f8bef5fe0a
Viet Nguyen <>
2024-12-05 19:55:28 +0700

    Create Login Controller Function

--- backend/controllers/UserController.js
+++ backend/controllers/UserController.js
@@ -2,6 +2,7 @@ const asyncHandler = require("express-async-handler");
 const dotenv = require("dotenv");
 const User = require("../models/UserModel");
 const jwt = require("jsonwebtoken");
+const bcrypt = require("bcryptjs");
 
 dotenv.config();
 const generateToken = (id) => {
@@ -55,6 +56,7 @@ const registerUser = asyncHandler(async (req, res) => {
       email: user.email,
       token,
       photo: user.photo,
+      phone: user.phone,
       bio: user.bio,
     });
   } else {
@@ -65,43 +67,53 @@ const registerUser = asyncHandler(async (req, res) => {
 
 // Login User
 const loginUser = asyncHandler(async (req, res) => {
-  res.send("Login User");
-  // const { email, password } = req.body;
-  // // Validation
-  // if (!email || !password) {
-  //   res.status(400);
-  //   throw new Error("Please fill in all required fields");
-  // }
-  // // Check if user exists in the database or not
-  // const user = await User.findOne({ email });
-  // if (!user) {
-  //   res.status(400);
-  //   throw new Error("Invalid credentials");
-  // }
-  // // Check if password is correct
+  // res.send("Login User");
+  const { email, password } = req.body;
+  // Validation
+  if (!email || !password) {
+    res.status(400);
+    throw new Error("Please fill in all required fields");
+  }
+  // Check if user exists in the database or not
+  const user = await User.findOne({ email });
+  if (!user) {
+    res.status(400);
+    throw new Error("User does not exist, please register");
+  }
+  // Check if password is correct
   // const isMatch = await user.matchPassword(password);
-  // if (!isMatch) {
-  //   res.status(400);
-  //   throw new Error("Invalid credentials");
-  // }
-  // // Generate token
-  // const token = generateToken(user._id);
-  // // Send HTTP-only cookie
-  // res.cookie("token", token, {});
-  // console.log("User logged in: ", user);
-  // if (user) {
-  //   res.status(200).json({
-  //     _id: user._id,
-  //     name: user.name,
-  //     email: user.email,
-  //     token,
-  //     photo: user.photo,
-  //     bio: user.bio,
-  //   });
-  // } else {
-  //   res.status(400);
-  //   throw new Error("Invalid user data");
-  // }
+  console.log("user: ", user);
+  const isMatch = await bcrypt.compare(password, user.password);
+  console.log("isMatch: ", isMatch);
+  if (!isMatch) {
+    res.status(400);
+    throw new Error("Invalid credentials");
+  }
+  // Generate token
+  const token = generateToken(user._id);
+  // Send HTTP-only cookie
+  res.cookie("token", token, {
+    path: "/", // root path
+    expires: new Date(Date.now() + 3 * 60 * 60 * 1000), // 3 hours
+    httpOnly: true,
+    secure: true,
+    sameSite: "none",
+  });
+  console.log("User logged in: ", user);
+  if (user) {
+    res.status(200).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      photo: user.photo,
+      phone: user.phone,
+      bio: user.bio,
+      token
+    });
+  } else {
+    res.status(400);
+    throw new Error("Invalid user data");
+  }
 });
 
 module.exports = { registerUser, loginUser };
