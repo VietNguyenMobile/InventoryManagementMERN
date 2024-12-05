309c12b10848a669e412ccf302eb424ff8b88a92
Viet Nguyen <>
2024-12-05 11:33:36 +0700

    Create User Route

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const cors = require("cors");
+const UserRouter = require("./routes/UserRouter");
 
 dotenv.config();
 const app = express();
@@ -11,6 +12,9 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+
+// Routes Middleware
+app.use("/api/users", UserRouter);
 
 // Routes
 app.get("/", (req, res) => {
