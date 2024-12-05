8bc085fc9c16549ef28829bc89275e3cef431ffa
Viet Nguyen <>
2024-12-05 11:40:41 +0700

    Create User Controller

--- backend/routes/UserRouter.js
+++ backend/routes/UserRouter.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const router = express.Router();
-
-const registerUser = () => {};
+const { registerUser } = require("../controllers/UserController");
 
 router.post("/register", registerUser);
 
